9b138d2f8cb59e916ff4b2f0a2a138415236dcbe
Granit Dula <granitdula98@gmail.com>
2025-11-06 23:19:09 +0000

    feat: ai bot functionality

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,9 @@
 import express, { Request } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { InferenceClient } from "@huggingface/inference";
+
+dotenv.config();
 
 interface PromptResponseBody {
   data: {
@@ -16,16 +20,26 @@ interface PromptRequestBody {
 const app = express();
 const port = 3000;
 
+const hf = new InferenceClient(process.env.HUGGINGFACEHUB_API_KEY);
+
 app.use(express.json());
 app.use(cors());
 
 app.post(
   "/llm/prompt",
-  (req: Request<undefined, PromptResponseBody, PromptRequestBody>, res) => {
-    console.log(req.body.data.prompt);
+  async (
+    req: Request<undefined, PromptResponseBody, PromptRequestBody>,
+    res
+  ) => {
+    const out = await hf.chatCompletion({
+      model: "HuggingFaceTB/SmolLM3-3B",
+      messages: [{ role: "user", content: req.body.data.prompt }],
+      max_tokens: 128,
+    });
+
     res.json({
       data: {
-        answer: "hardcoded ai response",
+        answer: out.choices[0].message.content ?? "",
       },
     });
   }
